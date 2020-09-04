4caf82ffd0763872268a8c848d816ccc89114165
mrafrazkhan <afrazkhan@pm.me>
2020-09-04 19:43:53 +0500

    First function done.

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+var {activityService} = require('./services/changeLastActivity');
+const readline = require("readline");
 
-MongoClient.connect(url, {useUnifiedTopology: true, useNewUrlParser: true},function(err, db) {
-    if (err) throw err;
-    const dbo = db.db('assignment');
-    const collection = dbo.collection('users');
-    
-    dbo.collection('users').countDocuments({}, function(err, data){
-        if(err){
-            console.log(err);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const start = async () =>{
+
+    console.log("\n--- PRESS 1: Update last activity for 10 random users.\n"+
+        "--- PRESS 2: Start worker.\n\nEnter choice: ");
+    for await (const line of rl) {
+        if (line === '1'){
+            await activityService.changeUsersActivity();
+            break;
         }
-        console.log(data);
-        db.close();
-    })
-  });
+        console.log('Worker started');
+        break;
+    }
+};
+start();
+
+rl.on("close", function() {
+    console.log("BYE BYE !!!");
+    process.exit(0);
+});
--- services/changeLastActivity.js
+++ services/changeLastActivity.js
@@ -1,35 +1,130 @@
 const {constants} = require('../helpers/constans');
 const MongoClient = require('mongodb').MongoClient;
-const fs = require('fs');
 
-var userEmails = [];
+/*
+    This function fecthes 10 random users from database.
+    
+    @return {object} return users array
+*/
+function fetchRandomUsers(){
 
-MongoClient.connect(constants.serverUrl,
-    {useUnifiedTopology: true, useNewUrlParser: true},
-        function(err, db) {
+    return new Promise((resolve, reject)=>{
 
-            if (err) throw err;
-            const dbo = db.db(constants.database);
-            const collection = dbo.collection(constants.collection);
-            const pipeline = [
-                {
-                    $match : {size: 10}
-                },
-                {
-                    $project: 
-                    {
-                        email:1,
-                        meta:
-                        {
-                            lastActivity:1
-                        }
+        // connecting to database
+        MongoClient.connect(constants.serverUrl,
+            {useUnifiedTopology: true, useNewUrlParser: true},
+                function(err, db) {
+        
+                    if (err){
+                        console.error('Error while fetching random users.');
+                        reject(err);
                     }
-                }
-            ];
+                    const dbo = db.db(constants.database);
+                    const collection = dbo.collection(constants.collection);
+                    
+                    // pipeline for fetching aggregated user result
+                    const pipeline = [
+                        {
+                            $sample : {size: 10}
+                        },
+                        {
+                            $project: 
+                            {
+                                email:1
+                            }
+                        }
+                    ];
+        
+                    // calling aggregate mongodb function and converting result
+                    // to an array. 
+                    collection.aggregate(pipeline).toArray((err, data)=>{
+                        if (err){
+                            console.error('Error while fetching random users.');
+                            reject(err);
+                        }
+                        db.close();
+                        resolve(data);
+                    });
+                });
+    });
+};
 
-            collection.aggregate(pipeline,(err, data)=>{
-                if(err) throw err
-                console.log(data)
-                db.close()
-            });
-  });
+/*
+    This function updates lastActivity field of all 10 fetched users.
+    
+    @param {object} Array of random 10 users fetched. 
+*/
+function updateUsersActivity(users){
+
+    return new Promise((resolve, reject)=>{
+
+        // connecting to database
+        MongoClient.connect(constants.serverUrl,
+            {useUnifiedTopology: true, useNewUrlParser: true},
+                function(err, db) {
+        
+                    if (err){
+                        console.error('Error while updating users activity.');
+                        reject(err);
+                    }
+                    const dbo = db.db(constants.database);
+                    const collection = dbo.collection(constants.collection);
+                    
+                    // A counter to detect end of loop through users array
+                    var updatedUsers = 0;
+
+                    try {
+                        users.forEach(user => {
+                        
+                            // Number of minutes randomly generated to subtract from current time. 
+                            const mins = Math.floor(Math.random() * 5) + 1;
+                            const currentTime = new Date().getTime();
+
+                            // Current time updated to generated random minutes ago 
+                            const updatedTime = new Date(currentTime-(mins*60000));
+                            
+                            // User last Activity update expression 
+                            const updateExp = {$set:{"meta.lastActivity" : updatedTime}};
+    
+                            // Updating single user activity on each iteration
+                            collection.updateOne({email: user['email']}, updateExp, (err, data)=>{
+
+                                if(err){
+                                    console.error('Error while updating users last activity.');
+                                    reject(err);            
+                                }
+
+                                updatedUsers++;
+                                console.log(`${updatedUsers}: "${user['email']}" last activity updated to ${mins}min ago.`);
+                                
+                                // closing connection and resolving promise if all users last activity updated.
+                                if (updatedUsers === users.length){
+                                    db.close();
+                                    resolve('Users activity updated successfully.');
+                                }
+                            })
+                        });   
+                    } catch (error) {
+                        console.error('Error while updating users last activity.');
+                        reject(error);
+                    };
+                });
+    });
+};
+
+async function changeUsersActivity(){
+
+    try {
+        var users = await fetchRandomUsers();
+        console.info('\n---------- 10 randome users fetched successfully.\n');
+        var updatedData = await updateUsersActivity(users);
+        console.info(`\n---------- ${updatedData}\n`);
+        return 'Last activity change process done.';
+    } catch (error) {
+        throw error;
+    }
+};
+
+exports.activityService = {
+    changeUsersActivity : changeUsersActivity
+};
